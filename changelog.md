0f159ca5d51c15ae880fca642f8f46ccfc00bfb7
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-05-10 15:20:20 +0300

    Added FirstComponent content

---
+++ changelog.md
@@ -0,0 +1,3 @@
+## Added top bar component
+## Created FirstComponent 
+It initialised with properties and used in AppComponent (Task 1.3, 1.4)
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Category { JavaScript, CSS, HTML, TypeScript, Angular9 }
+
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss']
 })
+
 export class FirstComponent implements OnInit {
+  name: string = 'First';
+  description: string = 'Some description of component';
+  tasks: Array<number> = [1, 2, 3];
+  tasksDescription: Array<string> = ['Task 1', 'Task 2', 'Task 3']
+  price: number = 100;
+  category: Category.Angular9;
+  available: boolean = true;
 
   constructor() { }
 
