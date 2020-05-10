e749d790cb2bf5ee87064a2c84a973c9e5fd650a
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-05-10 22:51:06 +0300

    Linting fixes

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -9,13 +9,15 @@ enum Category { JavaScript, CSS, HTML, TypeScript, Angular9 }
 })
 
 export class FirstComponent implements OnInit {
-  name: string = 'First';
-  description: string = 'Some description of component';
+  name = 'First'; // Adding type string (name: string) I see the linting error -
+  // "Type string trivially inferred from a string literal, remove type annotation".
+  // The same for lines 14, 17 (type number), 19 (type boolean)
+  description = 'Some description of component';
   tasks: Array<number> = [1, 2, 3];
-  tasksDescription: Array<string> = ['Task 1', 'Task 2', 'Task 3']
-  price: number = 100;
+  tasksDescription: Array<string> = ['Task 1', 'Task 2', 'Task 3'];
+  price = 100;
   category: Category.Angular9;
-  available: boolean = true;
+  available = true;
 
   constructor() { }
 
