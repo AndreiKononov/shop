656f5275a4db56ecc0af7edd00f3830776f35a54
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-05-17 22:41:49 +0300

    Task1. Comments resolving

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Product } from '../../product/models/product.model';
+import { Category } from '../../product/enums/category';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class CartService {
 
   getProducts(): Array<Product> {
     return [
-      new Product(1, 'Apple', 'A big green apple ', 3, true, true),
-      new Product(2, 'Orange', 'An usual orange', 8),
-      new Product(3, 'Banana', 'Just a simple yellow banana', 2, true),
-      new Product(4, 'Cherry', 'Just a red cherry', 1.5, true, true),
+      new Product(1, 'Apple', 'A big green apple ', 3, Category.Fruit, true, false, 5),
+      new Product(2, 'Orange', 'An usual orange', 8, Category.Fruit),
+      new Product(3, 'Banana', 'Just a simple yellow banana', 2, Category.Fruit, true),
+      new Product(4, 'Cherry', 'Just a red cherry', 1.5, Category.Fruit, true, false, 6),
     ];
   }
 }
--- src/app/product/models/product.model.ts
+++ src/app/product/models/product.model.ts
@@ -1,13 +1,17 @@
 /**
  * Product Model
  */
+import { Category } from '../enums/category';
+
 export class Product {
   constructor(
     public id: number = null,
     public name: string,
     public description: string,
     public price: number,
+    public category: Category,
     public available: boolean = true,
     public selected: boolean = false,
+    public amount: number = 0,
   ) {}
 }
